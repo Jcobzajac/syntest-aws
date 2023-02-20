309f6f4e66a2768fe85a61900f119d3e7de4c71e
Jakub Zajac <jakub.zajac@develeap.com>
2023-02-20 11:43:07 +0100

    Change parameters

--- syntest/lambda.tf
+++ syntest/lambda.tf
@@ -1,9 +1,9 @@
 module "lambda_function" {
   source                = "terraform-aws-modules/lambda/aws"
-  function_name         = var.lambda_function_name
-  description           = "Develeap bucket optimizer lambda"
-  handler               = "optimizer_lambda.lambda_handler" 
-  runtime               = "python3.8"
+  function_name         = "function name"
+  description           = "Syntest"
+  handler               = "handler" 
+  runtime               = "runtime"
   timeout = 30
-  source_path           = "./scripts/optimizer_lambda.py" #DYNAMIC 
+  source_path           = "path/script" 
 }
